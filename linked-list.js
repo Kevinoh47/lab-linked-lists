de1894a376c6d853110732b77d3b72b91bac4b25
Kevinoh47 <kohok47@gmail.com>
2018-10-18 12:20:45 -0700

    implemented prepend and reverse methods

--- linked-list.js
+++ linked-list.js
@@ -27,8 +27,49 @@ class LinkedList {
       current = current.next;
     }
 
-    // once we find the node with next is null, set next to the new node
+    // break out of the loop, and the current node's next is null. Set next to the new node
     current.next = node;
+    return this;
+  }
+
+  // prepend adds a new node to the beginning of the linked list
+  // Big O for time:  O(1)
+  // Big O for space: O(1)  
+  prepend(value) {
+    let node = new Node(value);
+
+    // first node for the linked list
+    if (! this.head) {
+      this.head = node;
+      return this;
+    }
+
+    // prepend node to the head
+    let newSecondNode = this.head;
+    this.head = node;
+
+    // reset linked list head
+    node.next = newSecondNode;
+
+    return this;
+  }
+
+  reverse() {
+    let previous = null;
+    let current = this.head;
+    let newNext = this.head.next;
+    
+    while (current.next) {
+      if (previous === null) {
+        previous = current; 
+        current.next = null; //this will now be the tail
+      }
+      else {
+        current.next = newNext;
+        newNext = previous;
+      }
+    }
+    
     return this;
   }
 }
